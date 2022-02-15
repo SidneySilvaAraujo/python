13fb6bca4b61a5c9044f6d10b2514bea577f3b13
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-02-15 18:05:24 -0300

    Adicionadas modificações no hello word.

--- alura/helloword.py
+++ alura/helloword.py
@@ -1,1 +1,7 @@
-print("Hello word!")
+primeiro = "hello"
+segundo = "word"
+print(primeiro, segundo, sep = "-", end = "!")
+
+#O parâmetro sep adiciona um - entre os dois valores
+#O parâmetro end, adiciona um ! no fim das strings concatenadas
+
