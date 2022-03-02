8e6a64aacb159d5698a4793e56645dcbf4c28626
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-03-02 12:15:34 -0300

    Jogo da forca em desemvolvimento

--- alura/forca.py
+++ alura/forca.py
@@ -1,5 +1,14 @@
 
 def jogar():
     print("Bem vindo ao jogo da forca!")
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    while (not acertou and not enforcou):
+        chute = str(input("qual letra?"))
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(chute)
     print("Fim de jogo.")
 
