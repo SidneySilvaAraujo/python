af093972a5ca1932241016971a4b0ee69591e34a
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-03-03 16:52:26 -0300

    Jogo da forca em produção

--- alura/forca.py
+++ alura/forca.py
@@ -6,9 +6,11 @@ def jogar():
     acertou = False
     while (not acertou and not enforcou):
         chute = str(input("qual letra?"))
+        chute = chute.strip()
         index = 0
         for letra in palavra_secreta:
-            if chute == letra:
-                print(chute)
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição: {}".format(chute, index))
+                index = index +1
     print("Fim de jogo.")
 
